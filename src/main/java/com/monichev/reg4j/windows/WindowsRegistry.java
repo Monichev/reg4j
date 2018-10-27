3a1f2d6021b14e167e78c63d64d52db4b7436ca2
Sergey Monichev <sergey.monichev@gmail.com>
2018-10-28 00:29:36 +0300

    Added a force flag to ADD and DELETE

--- src/main/java/com/monichev/reg4j/windows/WindowsRegistry.java
+++ src/main/java/com/monichev/reg4j/windows/WindowsRegistry.java
@@ -8,47 +8,16 @@ import com.monichev.reg4j.RegistryValue;
 
 public class WindowsRegistry implements Registry {
 
-    @Override
-    public RegistryValue get(String path, String key) {
-        String action = "query";
-        try {
-            WindowsRegistryAccessor provider = createRegistryAccessor(getCommand(path, key, action));
-
-            if (!provider.getError().isEmpty()) {
-                throw new RegistryException(action, path, key, provider.getError());
-            }
-
-            String[] outputEntries = provider.getOutput().split("\r\n");
-            String[] valueEntries = outputEntries[3].trim().replaceAll("\\s+", " ").split(" ");
-            return new WindowsRegistryValue(WindowsRegistryValue.Type.valueOf(valueEntries[1]), valueEntries[2]);
-        } catch (IOException e) {
-            throw new RegistryException(action, path, key, e);
-        }
-    }
-
-    @Override
-    public void add(String path, String key, RegistryValue value) {
-        String action = "add";
-        try {
-            WindowsRegistryAccessor provider = createRegistryAccessor(getCommand(path, key, action, value));
-            if (!provider.getError().isEmpty()) {
-                throw new RegistryException(action, path, key, provider.getError());
-            }
-        } catch (IOException e) {
-            throw new RegistryException(action, path, key, e);
-        }
-    }
-
-    private String getCommand(String path, String key, String action) {
+    private static String getCommand(String path, String key, RegAction action) {
         return getCommand(path, key, action, null);
     }
 
-    private String getCommand(String path, String key, String action, RegistryValue value) {
+    private static String getCommand(String path, String key, RegAction action, RegistryValue value) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(action);
         stringBuilder.append(" \"");
         stringBuilder.append(path);
-        stringBuilder.append("/v ");
+        stringBuilder.append("\" /v ");
         stringBuilder.append(key);
         if (value != null) {
             stringBuilder.append(" /t ");
@@ -56,24 +25,77 @@ public class WindowsRegistry implements Registry {
             stringBuilder.append(" /d ");
             stringBuilder.append(value.getValue());
         }
-        stringBuilder.append(" /f");
+        switch (action) {
+        case DELETE:
+        case ADD:
+            stringBuilder.append(" /f");
+            break;
+        default:
+            break;
+        }
         return stringBuilder.toString();
     }
 
     @Override
+    public RegistryValue get(String path, String key) {
+        RegAction action = RegAction.QUERY;
+        try {
+            WindowsRegistryAccessor provider = createRegistryAccessor(getCommand(path, key, action));
+
+            if (!provider.getError().isEmpty()) {
+                throw new RegistryException(action.toString(), path, key, provider.getError());
+            }
+
+            String[] outputEntries = provider.getOutput().split("\r\n");
+            String[] valueEntries = outputEntries[3].trim().replaceAll("\\s+", " ").split(" ");
+            return new WindowsRegistryValue(WindowsRegistryValue.Type.valueOf(valueEntries[1]), valueEntries[2]);
+        } catch (IOException e) {
+            throw new RegistryException(action.toString(), path, key, e);
+        }
+    }
+
+    @Override
+    public void add(String path, String key, RegistryValue value) {
+        RegAction action = RegAction.ADD;
+        try {
+            WindowsRegistryAccessor provider = createRegistryAccessor(getCommand(path, key, action, value));
+            if (!provider.getError().isEmpty()) {
+                throw new RegistryException(action.toString(), path, key, provider.getError());
+            }
+        } catch (IOException e) {
+            throw new RegistryException(action.toString(), path, key, e);
+        }
+    }
+
+    @Override
     public void delete(String path, String key) {
-        String action = "delete";
+        RegAction action = RegAction.DELETE;
         try {
             WindowsRegistryAccessor provider = createRegistryAccessor(getCommand(path, key, action));
             if (!provider.getError().isEmpty()) {
-                throw new RegistryException(action, path, key, provider.getError());
+                throw new RegistryException(action.toString(), path, key, provider.getError());
             }
         } catch (IOException e) {
-            throw new RegistryException(action, path, key, e);
+            throw new RegistryException(action.toString(), path, key, e);
         }
     }
 
     protected WindowsRegistryAccessor createRegistryAccessor(String command) throws IOException {
         return new WindowsRegistryAccessor(command);
+    }
+
+    private enum RegAction {
+        QUERY,
+        ADD,
+        DELETE,
+        COPY,
+        SAVE,
+        RESTORE,
+        LOAD,
+        UNLOAD,
+        COMPARE,
+        EXPORT,
+        IMPORT,
+        FLAGS;
     }
 }
